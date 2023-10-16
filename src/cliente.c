acfab6dfc781f4bd3351f79fe916cfb825fbfe54
Pedro Garcia <pedrognunes139@gmail.com>
2023-10-16 19:33:21 -0300

    final version

--- src/cliente.c
+++ src/cliente.c
@@ -1,39 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>//close
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-
-#define PORTA_PADRAO 4343
-
-#define INICIO_MATRIZ  1
-#define INICIO_TAM_MSG 10
-#define INICIO_MSG     12
-
-
-//Definindo um tipo boolean;
-typedef enum boolean{false, true}bool;
-
-//Exibe mensagem de erro e fornece um retorno;
-int Erro(char mensagem[], int);
-
-//Estabelece Conexao com o Servidor
-void IniciaSocket(int *, char ip[]);
-
-//Verifica se a coordenada foi recusada;
-bool VerificaRecuso(char c);
-
-//Verifica se o servidor enviou uma mensagem de fim de jogo;
-bool AnalisaFimPacote(char pacote[]);
-
-void Desencapsular(char pacote[], char matriz_jogo[], char mensagem_servidor[]);
-int  ConvertChar  (char);
-void CharParaInt  (char pacote[], int *);
-void ImprimirRecebido(char matriz_jogo[], char mensagem_servidor[]);
+#include "../include/cliente.h"
 
 int main(int argc,char *argv[]){
 	int  sock_programa;
--- src/servidor.c
+++ src/servidor.c
@@ -1,53 +1,5 @@
-#include <stdio.h>      //Para uso do entrada e saida de dados em alto nivel;
-#include <stdlib.h>     //Para uso do "system()"
-#include <string.h>     //Manipulacao de strings em geral: "strcpy()", "strcmp()", "strcat()"
-#include <unistd.h>     //Para a uso da funcao "close()"
-#include <sys/types.h>  //Funcoes de temporizacao
-#include <sys/socket.h> //Uso e manipulacao de Sockets
-#include <netinet/in.h> //Define a estrutura "sockaddr_in"
-#include <arpa/inet.h>  //Disponibiliza o tipo in_port_t e in_addr_t para uso da <netinet/in.h>
-#include <netdb.h>
+#include "../include/servidor.h"
 
-
-#define PORTA_PADRAO 4343
-
-//Definindo um tipo boolean;
-typedef enum boolean{false, true}bool;
-
-// Variáveis globais
-int Matriz_Jogo    [3][3];  // Matriz representando o jogo da velha
-int Coord_Digitadas[9];     // Coordenadas já escolhidas no jogo
-int jogada;                 // Contador de jogadas
-
-
-int  Erro(char mensagem[], int returno);         //Funcao generica para impressar de mensagem de erro e retorno de inteiro
-void StatusJogo(int, int, int, int);             //Imprime no tela, do servidor o status do jogo: matriz, jogada, realizador da jogada;
-
-/*Funcoes para estabelecimento de conexao  */
-bool IniciaSocketServidor(int *);               // Cria o Socket do Servidor, vincula e ouve uma porta[funções: bind() e listen];
-bool IniciaSocketClientes(int *, int *, int *); // Aceita os clientes[funcao: accept()];
-
-/*Funcoes para formatacao do pacote enviado*/
-void Encapsular      (char *, int);             // Gera um pacote para ser enviado
-void EncapsularMatriz(char pacote[]);           // Encapsula matriz no jogo, funcao chamada por "Encapsular()"
-void IntParaChar     (int, char char_gerado[]); // Converte um inteiro > 9 em um char, ex. int n = 129102 -> char m[7] ={"129102"};
-int  CharParaInt     (char);                    // Converte um char em um inteiro;
-char GetCaracter     (int);                     // Obtem um caractere a partir da tabela ASCII, ex. int n = 48 -> char m = '1';
-
-/* Função principal que trata de intercalar os clientes realizar uma jogada, reenvia o pacote solicitando uma nova jogada quando
-  invalida e determina o vencedor */
-void JogadaClientes(int *, int *, int *, int *, int *);
-void ImprimeMatriz(void);
-
-/*Funções para manipulacao da matriz do jogo*/
-bool ValidaCoordenada      (int coordenada);            // Verifica se a coordenada solicitada esta entre 1 e 9 e tambem se nao ja foi digitada anteriormente
-bool VerifCoordJaDigitada  (int aux);                   // Usada para verificar se a coordenada ja foi digitada
-int  VerificaFimMatriz     (int jogadas);               // Verifica se ja se tem um ganhador ou ocorreu um empate
-int  SomaCoordenadas       (int x, int y, int z);       // Na matriz eh preenchido com valores 1 e -1 dependendo do jogador, entao essa funcao soma os
-                                                        //valores para posteriormente verificar se ha a soma eh 3 ou -3, significando que ha um ganhador
-bool VerificaSomaCoord     (int aux);                   // Verifica se a soma eh 3 ou -3
-void ConverteEmCoordenadaXY(int coord, int *i, int *j); // Como as coordenadas sao solicitada por numeros de 1 a 9, essa funcao converte em coordenadas
-                                                        //i e j para alterar a matriz do jogo;
 
 int main(){
 	int sock_programa, i, j, cont=0;
